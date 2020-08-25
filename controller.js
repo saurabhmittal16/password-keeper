591810d22ac0711ddd7f5500a409b10b43829a0b
Saurabh Mittal <saurabhmittal1606@gmail.com>
2020-08-25 17:57:21 +0530

    Add user ID check to new password save

--- controller.js
+++ controller.js
@@ -6,9 +6,9 @@ exports.signup = (req, res) => {
     const { username, password } = req.body;
 
     // hash password before storing
-    const hashPasswprd = bcrypt.hashSync(password);
+    const hashPassword = bcrypt.hashSync(password);
 
-    connection.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, hashPasswprd], (err) => {
+    connection.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, hashPassword], (err) => {
         if (err) {
             console.log(err);
             res.status(500);
@@ -81,24 +81,35 @@ exports.addPassword = (req, res) => {
     const userId = parseInt(req.query.user);
     const { username, password, website } = req.body;
 
-    // encrypt the password before saving in the db
-    const encryptedPassword = encrypt(password);
+    // check if user with given id exists
+    connection.query("SELECT * FROM users WHERE id=?", [userId], (err, result) => {
+        if (err || result.length === 0) {
+            console.log(err);
+            res.status(500);
+            res.send({
+                message: "no such user",
+            });
+        } else {
+            // encrypt the password before saving in the db
+            const encryptedPassword = encrypt(password);
 
-    connection.query(
-        "INSERT INTO passwords (user_id, website, username, password) VALUES (?, ?, ?, ?)",
-        [userId, website, username, encryptedPassword],
-        (err) => {
-            if (err) {
-                console.log(err);
-                res.status(500);
-                res.send({
-                    message: "some error occured",
-                });
-            } else {
-                res.send({
-                    status: "success",
-                });
-            }
+            connection.query(
+                "INSERT INTO passwords (user_id, website, username, password) VALUES (?, ?, ?, ?)",
+                [userId, website, username, encryptedPassword],
+                (error) => {
+                    if (err) {
+                        console.log(error);
+                        res.status(500);
+                        res.send({
+                            message: "some error occured",
+                        });
+                    } else {
+                        res.send({
+                            status: "success",
+                        });
+                    }
+                }
+            );
         }
-    );
+    });
 };
