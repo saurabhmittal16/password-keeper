bf61f01d39885343d97303d024409077e3fbe794
Saurabh Mittal <saurabhmittal1606@gmail.com>
2020-08-25 15:56:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+const express = require("express");
+
+var app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 8000;
+
+// MySQL details
+var mysqlConnection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
+
+mysqlConnection.connect((err) => {
+    if (!err) console.log("Connection Established Successfully");
+    else console.log("Connection Failed!" + JSON.stringify(err, undefined, 2));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
