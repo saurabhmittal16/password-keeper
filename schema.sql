80551b5b5409cb929895f118b2e0383385eced9a
Saurabh Mittal <saurabhmittal1606@gmail.com>
2020-08-25 18:17:45 +0530

    Rename file

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `users` (
+    `id` BIGINT UNSIGNED AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `passwords` (
+    `id` BIGINT UNSIGNED AUTO_INCREMENT,
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    `website` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    PRIMARY KEY(`id`)
+);
